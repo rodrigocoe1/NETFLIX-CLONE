cc36ea07febfcde8ea6ce609a756ef64ca179baf
unknown <rodrigocoe2@gmail.com>
2022-06-23 15:39:59 -0300

    recomendado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Row from './components/row/row';
 import { categories } from './api';
+import Banner from './components/banner/banner';
+
 function App() {
   return (
     <div className="App">
+      <Banner/>
         {categories.map((category)=>{
           return<Row
             key={category.name}
